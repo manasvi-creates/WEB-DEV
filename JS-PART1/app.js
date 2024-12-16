d0386dd2adef6785149419a197aa0206a8b17065
Manasvi Garg <gargmanasvi79@gmail.com>
2024-12-16 20:53:51 +0530

    .
    .

---
+++ JS-PART1/app.js
@@ -0,0 +1,10 @@
+// console.log("Hello World!")
+// console.log("Manasvi garg")
+// let a = 10;
+// let b = 20;
+// console.log("sum is : ", a+b );
+let pencilPrice =10;
+let eraserPrice =5;
+// let output ="the total price is : "+( pencilPrice +eraserPrice)+" Rupees.";
+let output = `The total price is : ${pencilPrice+eraserPrice} Rupees.`
+console.log(output);
